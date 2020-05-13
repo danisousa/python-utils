0eb666e736692a5bdc1666ee0c004e064e5c0767
Daniel <sousadgsou@gmail.com>
2020-05-13 16:02:51 +0200

    fix exists

--- maven_arango.py
+++ maven_arango.py
@@ -11,50 +11,54 @@ import time
 # Create document of repository in database
 def create_maven_repo_document(repo, connection):
     
+    connection.reload()
     repositoryCollection = connection['CATALOG']['REPOSITORY']
-
+    
     try:
-        # Fill information of document
-        docu = repositoryCollection.createDocument()
-        docu._key = repo['id']
-        docu["origin"] = repo['origin']
-        docu["name"] = repo['name']
-        docu["packing_type"] = repo['packing_type']
-        docu.save()
+        if(check_docu_exists('REPOSITORY', repo['id'] ) == 0):
+            # Fill information of document
+            docu = repositoryCollection.createDocument()
+            docu._key = repo['id']
+            docu["origin"] = repo['origin']
+            docu["packing_type"] = repo['packing_type']
+            docu.save()
     except:
-        print('Creation error in repo: {}'.format(repo['name']))
+        print('Creation error in repo: {}'.format(repo['id']))
         
     
 # Create document of dependency in database
 def create_maven_depend_document(dep, connection):
     
-    # Get or create collection from DB
+    connection.reload()
     dependencyCollection = connection['CATALOG']['DEPENDENCY']
-        
+
     try:
-        # Fill information of document
-        docu = dependencyCollection.createDocument()
-        docu._key = dep['id']
-        docu["origin"] = dep['origin']
-        docu["name"] = dep['name']
-        docu["version"] = dep['version']
-        docu["packing_type"] = dep['packing_type']
-        docu.save()
+        
+        if(check_docu_exists('DEPENDENCY', dep['id'] ) == 0):
+            # Fill information of document
+            docu = dependencyCollection.createDocument()
+            docu._key = dep['id']
+            docu["origin"] = dep['origin']
+            docu["name"] = dep['name']
+            docu["version"] = dep['version']
+            docu["packing_type"] = dep['packing_type']
+            docu.save()
     except:
         print('Creation error in dependency: {}'.format(dep['artifact']))
     
 # Create document of relationship in database
 def create_maven_edge_document(dep, types, connection):
     
-    # Get or create collection from DB
+    connection.reload()
     relationsCollection = connection['CATALOG']['CONTAINS']
     
     # Fill information of document
     edge = relationsCollection.createEdge()
-    edge._from = '{}/{}'.format(types['sourceType'], dep['source'])
-    edge._to = '{}/{}'.format(types['targetType'], dep['target'])
+    edge._from = '{}/{}'.format(types['sourceType'], dep['sourceID'])
+    edge._to = '{}/{}'.format(types['targetType'], dep['targetID'])
     edge["branch"] = 'master'
-    edge.save()
+    if(check_edge_exists('CONTAINS', edge) == 0):
+        edge.save()
 	
 # Find if the edge is between repository or component
 def findType(tree, elements):
@@ -83,7 +87,34 @@ def findType(tree, elements):
 
     return types
 
-# Method to read the GML file
+
+def find_ids(tree, elements):
+    types = {"sourceID":"", "targetID":""}
+    types = json.loads(json.dumps(types))
+    
+    for elem in tree.iter():
+        if(elem.attrib.get('id') == elements['source']):
+            for subelem in elem.iter():
+                if(subelem.tag.split('}')[1] == 'NodeLabel'):
+                    info = subelem.text.split(':')
+                    if(len(info) == 4):
+                        types['sourceID'] = info[1]
+                    else:
+                        new_id = info[1]+'@'+info[3]
+                        types['sourceID'] = new_id
+                        
+        if(elem.attrib.get('id') == elements['target']):
+            for subelem in elem.iter():
+                if(subelem.tag.split('}')[1] == 'NodeLabel'):
+                    info = subelem.text.split(':')
+                    if(len(info) == 4):
+                        types['targetID'] = info[1]
+                    else:
+                        new_id = info[1]+'@'+info[3]
+                        types['targetID'] = new_id
+
+    return types
+
 def read_GML(path_to_file, conn):
 
     # Parse the dependency tree
@@ -103,12 +134,13 @@ def read_GML(path_to_file, conn):
                     # If it is the repo info
                     if(len(info) == 4):
                         # Create repo document
-                        docu = {"id":id, "origin":info[0], "name":info[1], "packing_type":info[2]}
+                        docu = {"id":info[1], "origin":info[0], "packing_type":info[2]}
                         docu = json.loads(json.dumps(docu))
                         create_maven_repo_document(docu, conn)
                     elif(len(info) == 5):
                         # Create dependency Document
-                        docu = {"id":id, "origin":info[0], "name":info[1], "packing_type":info[2], "version":info[3]}
+                        dep_id = info[1]+'@'+info[3]
+                        docu = {"id":dep_id, "origin":info[0], "name":info[1], "packing_type":info[2], "version":info[3]}
                         docu = json.loads(json.dumps(docu))
                         create_maven_depend_document(docu, conn)
 
@@ -116,12 +148,35 @@ def read_GML(path_to_file, conn):
         if(elem.tag.split('}')[1] == 'edge'):
             atributos = json.loads(json.dumps(elem.attrib))
             types = findType(tree, atributos)
-            create_maven_edge_document(atributos, types, conn)
-			
+            elems_ids = find_ids(tree, atributos)
+            create_maven_edge_document(elems_ids, types, conn)
+            
+# Check if document already exists in DB
+def check_docu_exists(docu_type, docu_id):
+    aql = "FOR doc IN {} FILTER doc._id == '{}' RETURN doc._id".format(docu_type, docu_type+'/'+docu_id)
+    query_result = arango['CATALOG'].AQLQuery(aql, rawResults=True, batchSize=100)
+    if(len(query_result) > 0):
+        return 1
+    else:
+        return 0
+    
+# Check if edge already exists in DB
+def check_edge_exists(docu_type, edge):
+    aql = "FOR doc IN {} FILTER doc._from == '{}' and doc._to == '{}' and doc.branch == '{}' RETURN doc._id".format(docu_type, 
+                                                                                                                   edge['_from'], 
+                                                                                                                   edge['_to'], 
+                                                                                                                   edge['branch'])
+    query_result = arango['CATALOG'].AQLQuery(aql, rawResults=True, batchSize=100)
+    if(len(query_result) > 0):
+        return 1
+    else:
+        return 0
+        
 # Connection to ArangoDB
 arango = Connection(arangoURL='http://localhost:8529', username="root", password="root")
 # Clean the DB
-arango['CATALOG'].dropAllCollections()
+#arango['CATALOG'].dropAllCollections()
+#arango['CATALOG'].reload()
 
 if (not arango['CATALOG'].hasCollection('CONTAINS')):
     arango['CATALOG'].createCollection(name='CONTAINS', className='Edges', waitForSync=True)
@@ -135,7 +190,7 @@ if (not arango['CATALOG'].hasCollection('REPOSITORY')):
     arango['CATALOG'].createCollection(name='REPOSITORY')
     print("Creating Collection <REPOSITORY>")
 
-arango.reload()
+arango['CATALOG'].reload()
 
 # Begin
 read_GML('out.gml', arango)
---
+++ node_arango.py
@@ -0,0 +1,127 @@
+import json
+from pyArango.connection import *
+import time
+
+# Create document of repository in database
+def create_node_repo_document(repo, connection):
+    
+    repositoryCollection = connection['CATALOG']['REPOSITORY']
+
+    try:
+        if(check_docu_exists('REPOSITORY', repo['name'] ) == 0):
+            # Fill information of document
+            docu = repositoryCollection.createDocument()
+            docu._key = repo['name']
+            docu["version"] = repo['version']
+            docu.save()
+    except:
+        print('Creation error in repo: {}'.format(repo))
+        
+# Create document of dependency in database
+def create_node_depend_document(document, connection):
+        
+    dependencyCollection = connection['CATALOG']['DEPENDENCY']
+
+    try:
+        key = check_key_format(document['from'])
+
+        if(check_docu_exists('DEPENDENCY', key ) == 0):
+            # Fill information of document
+            docu = dependencyCollection.createDocument()
+            docu._key = key
+#            docu["origin"] = document['resolved']
+            docu["version"] = document['version']
+            docu.save()
+    except:
+        print('Creation error in dependency: {}'.format(document))
+        print('Creation error in document: {}'.format(docu))
+    
+# Create document of relationship in database
+def create_node_edge_document(parent, origin, parent_type, connection):
+
+    relationsCollection = connection['CATALOG']['CONTAINS']
+    
+    try:
+        parent = check_key_format(parent)
+        origin_key = check_key_format(origin['from'])
+        
+        # Fill information of document
+        edge = relationsCollection.createEdge()
+        edge._from = '{}/{}'.format(parent_type, parent)
+        edge._to = '{}/{}'.format('DEPENDENCY', origin_key)
+        edge["branch"] = 'master'
+        if(check_edge_exists('CONTAINS', edge) == 0):
+            edge.save()
+    except:
+        print('Creation error in edge: {}'.format(edge))
+
+# Check if document already exists in DB
+def check_docu_exists(docu_type, docu_id):
+    aql = "FOR doc IN {} FILTER doc._id == '{}' RETURN doc._id".format(docu_type, docu_type+'/'+docu_id)
+    query_result = arango['CATALOG'].AQLQuery(aql, rawResults=True, batchSize=100)
+    if(len(query_result) > 0):
+        return 1
+    else:
+        return 0
+    
+# Check if edge already exists in DB
+def check_edge_exists(docu_type, edge):
+    aql = "FOR doc IN {} FILTER doc._from == '{}' and doc._to == '{}' and doc.branch == '{}' RETURN doc._id".format(docu_type, 
+                                                                                                                   edge['_from'], 
+                                                                                                                   edge['_to'], 
+                                                                                                                   edge['branch'])
+    query_result = arango['CATALOG'].AQLQuery(aql, rawResults=True, batchSize=100)
+    if(len(query_result) > 0):
+        return 1
+    else:
+        return 0
+    
+# Check if key has '/'
+def check_key_format(key):
+    clave = key
+    if("/" in key):
+        clave = clave.replace("/", ":")
+    return clave
+    
+def get_depend_depth(source, parent, from_type, conn):
+        
+    # Check if it has dependencies at this level
+    if ('dependencies' in source):
+        deps = source['dependencies']
+
+        # For each dependency
+        for dep in deps:
+            create_node_depend_document(deps[dep], conn)
+            create_node_edge_document(parent, deps[dep], from_type, conn)
+            get_depend_depth(deps[dep], deps[dep]['from'], 'DEPENDENCY', conn)
+            
+def read_JSON(json_path, conn):
+
+    with open(json_path) as f:
+        data = json.load(f)
+
+        create_node_repo_document(data, conn)
+        get_depend_depth(data, data['name'], 'REPOSITORY', conn)
+        
+# Connection to ArangoDB
+arango = Connection(arangoURL='http://localhost:8529', username="root", password="root")
+# Clean the DB
+#arango['CATALOG'].dropAllCollections()
+#arango['CATALOG'].reload()
+
+if (not arango['CATALOG'].hasCollection('CONTAINS')):
+    arango['CATALOG'].createCollection(name='CONTAINS', className='Edges', waitForSync=True)
+    print("Creating Edge <CONTAINS>")
+    
+if (not arango['CATALOG'].hasCollection('DEPENDENCY')):
+    arango['CATALOG'].createCollection(name='DEPENDENCY')
+    print("Creating Collection <DEPENDENCY>")
+    
+if (not arango['CATALOG'].hasCollection('REPOSITORY')):
+    arango['CATALOG'].createCollection(name='REPOSITORY')
+    print("Creating Collection <REPOSITORY>")
+
+arango['CATALOG'].reload()
+
+# Begin
+read_JSON('outfile.json', arango)
