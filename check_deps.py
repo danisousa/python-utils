2246efaa6f566d326ac54b959789b7a09dd1610a
Daniel <sousadgsou@gmail.com>
2020-05-19 15:28:37 +0200

    less print

--- check_deps.py
+++ check_deps.py
@@ -15,7 +15,6 @@ def get_path(repo_name, dependency, arango):
     aql = "FOR v, e IN ANY SHORTEST_PATH 'REPOSITORY/{}' TO '{}' CONTAINS RETURN v._key".format(repo_name, dependency, arango)
     query_result = arango['CATALOG'].AQLQuery(aql, rawResults=True, batchSize=100)
     if(len(query_result) > 0):
-        print("\n")
         print("NOT VALIDATED DEPENDENCY: {}".format(dependency))
         for step in query_result:
             indent= i*"\t"
@@ -25,8 +24,9 @@ def get_path(repo_name, dependency, arango):
                 print("{}{}".format(indent, "|"))
                 print("{}{}".format(indent, step))
             i = i + 1
-    
+        print("")
+        
 # Connection to ArangoDB
 arango = Connection(arangoURL='http://192.168.56.1:8529', username="root", password="root")
 #get_repo_dependencies('sample-project-maven', arango)
-check_fails('sample-project-maven', arango)
+check_fails(os.environ['REPO_NAME'], arango)
