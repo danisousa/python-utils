f16f828adc16db5b1f4efbb9f4e810494b68f03a
Daniel <sousadgsou@gmail.com>
2020-05-19 17:44:43 +0200

    try

--- check_deps.py
+++ check_deps.py
@@ -6,25 +6,32 @@ import os
 def check_fails(repo_name, arango):
     aql = "FOR doc IN DEPENDENCY FILTER doc.validated == false RETURN doc._id"
     query_result = arango['CATALOG'].AQLQuery(aql, rawResults=True, batchSize=100)
+
     if(len(query_result) > 0):
         for dep in query_result:
-            get_path(repo_name, dep, arango)
+            # Get depth of path
+            aql = "RETURN LENGTH(FOR v IN ANY SHORTEST_PATH 'REPOSITORY/{}' TO '{}' CONTAINS RETURN v)".format(repo_name, dep)
+            query_result = arango['CATALOG'].AQLQuery(aql, rawResults=True, batchSize=100)
+            
+            # If exists a path
+            if(len(query_result) > 0):
+                get_path(repo_name, dep, query_result[0], arango)
         
-def get_path(repo_name, dependency, arango):
+def get_path(repo_name, dependency, depth, arango):
     i=1
-    aql = "FOR v, e IN ANY SHORTEST_PATH 'REPOSITORY/{}' TO '{}' CONTAINS FILTER e.branch=='{}' RETURN v._key".format(repo_name, dependency, os.environ['BRANCH'], arango)
+    aql = "FOR v, e, p IN {} OUTBOUND 'REPOSITORY/{}' CONTAINS FILTER v._id == '{}' && e.branch == '{}' && v.validated == false RETURN p.edges".format(depth-1, repo_name, dependency, os.environ['BRANCH'], arango)
     query_result = arango['CATALOG'].AQLQuery(aql, rawResults=True, batchSize=100)
     if(len(query_result) > 0):
         print("NOT VALIDATED DEPENDENCY: {}".format(dependency))
-        for step in query_result:
+        print(repo_name)
+        for step in query_result[0]:
             indent= i*"\t"
             if(step==repo_name):
                 print("Repository: {}".format(step))
             else:
                 print("{}{}".format(indent, "|"))
-                print("{}{}".format(indent, step))
+                print("{}{}".format(indent, step['_to']))
             i = i + 1
-        print("")
         
 # Connection to ArangoDB
 arango = Connection(arangoURL='http://192.168.56.1:8529', username="root", password="root")
